cc4ea887220750eff7d2b0bdc465dcebbaf410d3
mercik <muhittinercik@gmail.com>
2020-02-29 22:24:40 -0600

    update

--- src/day1/LoopPractice.java
+++ src/day1/LoopPractice.java
@@ -3,8 +3,9 @@ package day1;
 public class LoopPractice {
     public static void main(String[] args) {
 
-        for(int i = 0; i < 10; i+=2) {
-            System.out.println("Counter : "+i);
+        for (int i = 0; i < 10; i += 2) {
+            System.out.println("Counter : " + i);
+            System.out.println("@@@@@@@@@@");
         }
     }
 }
